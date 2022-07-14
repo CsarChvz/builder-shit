65405b3de803b6eaa07129a8489ec36d2f7fb3bd
CsarChvz <cesarchavezrodriguez9@gmail.com>
2022-07-14 19:29:53 +0000

    MenuWithAvatar and Header components -- Fixed

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -34,7 +34,7 @@ class MyApp extends App {
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           </Head>
           <CssBaseline />
-          <Header />
+          <Header {...pageProps} />
           <Component {...pageProps} />
         </ThemeProvider>
       </CacheProvider>
